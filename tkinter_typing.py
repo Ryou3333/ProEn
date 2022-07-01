103e8d310565243a3ad509e87007332e7dc0d105
38120016 <bb38120016@ms.nagasaki-u.ac.jp>
2022-07-01 15:58:02 +0900

    一文字タイピング～挑戦1～

--- tkinter_typing.py
+++ tkinter_typing.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-#TKクラス生成
+'''#TKクラス生成
 root = tk.Tk()
 #画面サイズ
 root.geometry('600x400')
@@ -16,4 +16,22 @@ text.place(x=90, y=70)
 #text.delete(0, tk.END)
 #text.insert(tk.END,"1234")
 #画面をそのまま表示
-root.mainloop()
+root.mainloop()'''
+
+class Application(tk.Frame):
+    def _init_(self, game):
+        super()._init_(game)
+        self.pack()
+
+        game.geometry("600x400")
+        game.title("一文字タイピング")
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.q_label = tk.Label(self, text="問題：", font=("",20))
+        self.q_label.grid(row=0, column=0)
+        self.q_labe2 = tk.Label(self, text="tkinter", width=5, anchor="w", font=("",20))
+        self.q_labe2.grid(row=0, column=1)
+        self.ans_label = tk.Label(self, text="解答：", font("",20))
+        self.ans_label.grid(row=1, column=0)
