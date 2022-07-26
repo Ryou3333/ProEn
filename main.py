5e031a9d5378cb6dfd5e287a5837208d23b86283
38120016 <bb38120016@ms.nagasaki-u.ac.jp>
2022-07-26 16:34:04 +0900

    画面切り替え適用できない

--- main.py
+++ main.py
@@ -1,56 +1,3 @@
-'''
-import tkinter as tk
-import tkinter.ttk as ttk
-
-def change_app():
-    frame_app.tkraise()
-
-def change_main():
-    frame.tkraise()
-
-if __name__ == "__main__":
-    # rootメインウィンドウの設定
-    root = tk.Tk()
-    root.title("tkinter application")
-    root.geometry("600x400")
-
-    # rootメインウィンドウのグリッドを 1x1 にする
-    root.grid_rowconfigure(0, weight=1)
-    root.grid_columnconfigure(0, weight=1)
-
-    # メインフレームの作成と設置
-    frame = ttk.Frame(root)
-    frame.grid(row=0, column=0, sticky="nsew", pady=20)
-
-    # 各種ウィジェットの作成
-    label1_frame = ttk.Label(frame, text="メインウィンドウ")
-    entry1_frame = ttk.Entry(frame)
-    button_change = ttk.Button(frame, text="アプリウィンドウに移動", command=change_app)
-
-    # 各種ウィジェットの設置
-    label1_frame.pack()
-    entry1_frame.pack()
-    button_change.pack()
-
-    # アプリフレームの作成と設置
-    frame_app = ttk.Frame(root)
-    frame_app.grid(row=0, column=0, sticky="nsew", pady=20)
-
-    # 各種ウィジェットの作成
-    label1_frame_app = ttk.Label(frame_app, text="アプリウィンドウ")
-    entry1_frame_app = ttk.Entry(frame_app)
-    button_change_frame_app = ttk.Button(frame_app, text="メインウィンドウに移動", command=change_main)
-
-    # 各種ウィジェットの設置
-    label1_frame_app.pack()
-    entry1_frame_app.pack()
-    button_change_frame_app.pack()
-
-    # frameを前面にする
-    frame.tkraise()
-
-    root.mainloop()
-'''
 import tkinter as tk
 import PIL.Image, PIL.ImageTk
 from tkinter import messagebox
@@ -59,7 +6,11 @@ import time
 import threading
 import random
 
-class App(tk.Tk):
+QUESTION = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+            "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+class Application(tk.Frame):
     # 呪文
     def __init__(self, *args, **kwargs):
         # 呪文
@@ -71,7 +22,6 @@ class App(tk.Tk):
         # ウィンドウの大きさを決定
         self.geometry("800x600")
 
-        '''
         self.index = 0
         self.index2 = random.randint(0,25)
 
@@ -86,7 +36,6 @@ class App(tk.Tk):
 
         #Tkインスタンスに対してキーイベント処理を実装
         self.master.bind("<KeyPress>", self.type_event)
-        '''
 
         # ウィンドウのグリッドを 1x1 にする
         # この処理をコメントアウトすると配置がズレる
@@ -106,13 +55,70 @@ class App(tk.Tk):
 #-----------------------------------frame1---------------------------------
         # 移動先フレーム作成
         self.frame1 = tk.Frame()
-        self.frame1.grid(row=0, column=0, sticky="nsew")
+        #self.frame1.grid(row=0, column=0, sticky="nsew")
+        def create_widgets(self):
+            self.q_label = tk.Label(self.frame1, text="問題：", font=("",20))
+            self.q_label.grid(row=0, column=0)
+            self.q_label2 = tk.Label(self.frame1, text=QUESTION[self.index2], width=10, anchor="w", font=("",20))
+            self.q_label2.grid(row=0, column=1)
+            self.ans_label = tk.Label(self.frame1, text="解答：", font=("",20))
+            self.ans_label.grid(row=1, column=0)
+            self.ans_label2 = tk.Label(self.frame1.frame1, text="", width=10, anchor="w", font=("",20))
+            self.ans_label2.grid(row=1, column=1)
+            self.result_label = tk.Label(self, text="", font=("",20))
+            self.result_label.grid(row=2, column=0, columnspan=2)
+
+            #時間計測用のラベル
+            self.time_label = tk.Label(self.frame1, text="", font=("",20))
+            self.time_label.grid(row=3, column=0, columnspan=2)
+
+            self.flg2 = True
+
+        #キー入力時のイベント処理
+        def type_event(self, event):
+            #入力値がEnterの場合は答え合わせ
+            if event.keysym == "Return":
+                if self.q_label2["text"] == self.ans_label2["text"]:
+                    self.result_label.configure(text="ピンポン！", fg="red")
+                    self.correct_cnt += 1
+                    i = 1
+                else:
+                    self.result_label.configure(text="ブブー！", fg="blue")
+
+                #解答欄をクリア
+                self.ans_label2.configure(text="")
+
+                #次の問題を出題
+                self.index += 1
+                if self.correct_cnt == 10:
+                    self.flg = False
+                    self.q_label2.configure(text="終了！")
+                    messagebox.showinfo("リザルト", f"あなたのスコアは{self.correct_cnt}/{self.index}問正解です。")
+                    sys.exit(0)
+
+                self.index2 = random.randint(0,14)
+                self.q_label2.configure(text=QUESTION[self.index2])
+
+            elif event.keysym == "BackSpace":
+                text = self.ans_label2["text"]
+                self.ans_label2["text"] = text[:-1]
+            else:
+                #入力値がEnter以外の場合は文字入力としてラベルに追記する
+                self.ans_label2["text"] += event.keysym
+
+        def timer(self):
+            self.second = 0
+            self.flg = True
+            while self.flg:
+                self.second += 1
+                self.time_label.configure(text=f"経過時間：{self.second}秒")
+                time.sleep(1)
         # タイトルラベル作成
-        self.titleLabel = tk.Label(self.frame1, text="Frame 1", font=('Helvetica', '35'))
-        self.titleLabel.pack(anchor='center', expand=True)
+        #self.titleLabel = tk.Label(self.frame1, text="Frame 1", font=('Helvetica', '35'))
+        #self.titleLabel.pack(anchor='center', expand=True)
         # フレーム1からmainフレームに戻るボタン
-        self.back_button = tk.Button(self.frame1, text="Back", command=lambda : self.changePage(self.main_frame))
-        self.back_button.pack()
+        #self.back_button = tk.Button(self.frame1, text="Back", command=lambda : self.changePage(self.main_frame))
+        #self.back_button.pack()
 #--------------------------------------------------------------------------
 
         #main_frameを一番上に表示
@@ -125,5 +131,5 @@ class App(tk.Tk):
         page.tkraise()
 
 if __name__ == "__main__":
-    app = App()
+    app = Application()
     app.mainloop()
