73a3a7cc0b4c7dab44009398ef76ee3b3998bc6c
38120016 <bb38120016@ms.nagasaki-u.ac.jp>
2022-07-28 14:39:05 +0900

    完成

--- game.py
+++ game.py
@@ -16,7 +16,6 @@ from scipy.spatial import distance
 import openpyxl as xl
 
 
-
 landmark_line_ids = [(0, 1), (1, 5), (5, 9), (9, 13), (13, 17), (17, 0),(1, 2), (2, 3), (3, 4), (5, 6), (6, 7), (7, 8),(9, 10), (10, 11), (11, 12), (13, 14), (14, 15), (15, 16), (17, 18), (18, 19), (19, 20)]
 
 mp_hands = mp.solutions.hands
